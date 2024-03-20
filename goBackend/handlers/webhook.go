02f939e3ad43c6e937067f637d8b542b6fdeb5e6
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-03-21 01:15:44 +0530

    Implemented webhook forward route handler which watches db for changes

--- goBackend/handlers/webhook.go
+++ goBackend/handlers/webhook.go
@@ -2,28 +2,91 @@ package handlers
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
+	"sync/atomic"
 
+	"github.com/SlightlyEpic/webhooked/models"
+	"github.com/SlightlyEpic/webhooked/services"
 	"github.com/gin-gonic/gin"
 )
 
+type WebhookInfoChangeEvent = services.DocumentChangeEvent[models.WebhookInfo]
+
 func (deps *HandlerDependencies) WebhookHandler() {
-	// * TODO: Make changestream events update the router paths
-	// ch := make(chan services.DocumentChangeEvent[models.WebhookInfo])
-
-	// deps.Db.AddWebhookWatcher(ch)
-	// fmt.Println("Added watcher...")
-
 	hooks, err := deps.Db.ActiveWebhooks()
 	if err != nil {
 		panic(err)
 	}
+
+	// Maps Id -> WebhookInfo
+	hookLookup := make(map[string]models.WebhookInfo)
+
+	// ~ Add existing hooks to lookup
 	for _, hook := range hooks {
-		// ! DEBUG: This creates a route on /webhook/ObjectID("65f9f34767ec6ff0884da2df")
-		deps.Router.POST(fmt.Sprintf("/webhook/%s", hook.Id), func(ctx *gin.Context) {
-			fmt.Printf("Recieved webhook %s\n", hook.Id)
-			// * TODO: Forward the webhook to the destination URL
-			// * TODO for later: Forward the webhook to all websocket recievers
-			// * TODO: Create a new log entry
-		})
+		hookLookup[hook.Id.Hex()] = hook
+	}
+
+	// ~ Respond to changes in db
+	ch := make(chan WebhookInfoChangeEvent)
+	deps.Db.AddWebhookWatcher(ch)
+	go deps.handleChangeEvent(ch, hookLookup)
+
+	// ~ http client that sends the POST requests to destination urls
+	httpClient := http.Client{}
+
+	// ~ Dynamic route that will forward webhook if such a WebhookInfo record exists, else return a 404
+	deps.Router.POST("/webhook/:webhookId", func(ctx *gin.Context) {
+		hookId := ctx.Param("webhookId")
+		hookInfo, ok := hookLookup[hookId]
+
+		if !ok {
+			ctx.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		successCount := atomic.Int32{}
+		wg := sync.WaitGroup{}
+		wg.Add(len(hookInfo.DestinationUrls))
+
+		for _, destUrl := range hookInfo.DestinationUrls {
+			go func() {
+				defer wg.Done()
+
+				req, err := http.NewRequest("POST", destUrl, ctx.Request.Body)
+				if err != nil {
+					fmt.Printf("/webhook/%s -> %s: Failed to create request\n", hookId, destUrl)
+					return
+				}
+
+				req.Header.Add("Content-Type", ctx.GetHeader("Content-Type"))
+
+				resp, err := httpClient.Do(req)
+				if err != nil {
+					fmt.Printf("/webhook/%s -> %s: %s\n", hookId, destUrl, err)
+					return
+				}
+
+				resp.Body.Close()
+				successCount.Add(1)
+			}()
+		}
+
+		wg.Wait()
+
+		// * TODO: Write logic to push log to db
+	})
+}
+
+func (deps *HandlerDependencies) handleChangeEvent(watcher <-chan WebhookInfoChangeEvent, hookLookup map[string]models.WebhookInfo) {
+	for evt := range watcher {
+		switch evt.OperationType {
+		case "insert", "update":
+			// fmt.Printf("New WebhookInfo: %s\n", evt.DocumentKey.Id.Hex())
+			hookLookup[evt.FullDocument.Id.Hex()] = evt.FullDocument
+		case "delete":
+			// fmt.Printf("Deleting WebhookInfo: %s\n", evt.DocumentKey.Id.Hex())
+			delete(hookLookup, evt.DocumentKey.Id.Hex())
+		}
 	}
 }
