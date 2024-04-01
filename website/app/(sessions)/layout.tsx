73fdb757505344258848a23dff2154d0f483ed59
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-04-01 23:05:10 +0530

    Add redux toolkit and create log slice

--- website/app/(sessions)/layout.tsx
+++ website/app/(sessions)/layout.tsx
@@ -1,17 +1,16 @@
 import { getServerSession } from 'next-auth';
 import { SessionProvider } from '@/components/SessionProvider';
+import { makeStore, type AppStore } from '@/lib/redux/store';
+import StoreProvider from '@/components/StoreProvider';
 
-
-export default async function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: Readonly<{children: React.ReactNode }>) {
     const session = await getServerSession();
 
     return (
         <SessionProvider session={session}>
-            {children}
+            <StoreProvider>
+                {children}
+            </StoreProvider>
         </SessionProvider>
     );
 }
