5ec5c11b48ddaa4aa470af07f6b187219bcb97e8
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2024-06-25 22:52:37 +0530

    deploy: fix home

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,34 +1,16 @@
-import { BackgroundBeams } from '@/components/aceternity/BackgroundBeams';
 import Typewrite from '@/components/home/Typewrite';
 import { ChevronDown, GithubIcon } from 'lucide-react';
 
 import './home.css';
 import { Button } from '@/components/shadcn/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/shadcn/ui/card';
-import { Boxes, BoxesCore } from '@/components/aceternity/BackgroundBoxes';
-import { cn } from '@/lib/utils';
-
-export function BackgroundBoxesDemo() {
-    return (
-        <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
-            <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-
-            <Boxes />
-            <h1 className={cn('md:text-4xl text-xl text-white relative z-20')}>
-                Tailwind is Awesome
-            </h1>
-            <p className="text-center mt-2 text-neutral-300 relative z-20">
-                Framer motion is the best animation library ngl
-            </p>
-        </div>
-    );
-}
+import { Boxes } from '@/components/aceternity/BackgroundBoxes';
+import Link from 'next/link';
 
 export default function App() {
     return <div className='w-full h-max relative flex flex-col items-center justify-center overflow-x-clip'>
-        {/* <BackgroundBeams className='-z-10' /> */}
-        <div className="fixed top-0 h-screen w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
-            <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+        <div className="absolute top-0 h-screen w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+            <div className="absolute inset-0 w-full h-full bg-slate-900 z-10 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
             <Boxes />
         </div>
 
@@ -39,13 +21,34 @@ export default function App() {
             </h3>
 
             <div className='mt-24 flex flex-col md:flex-row gap-8 justify-center items-center'>
-                <Button className='flex gap-2 button-glow transition-all hover:scale-105 duration-300 pointer-events-auto'>
-                    <GithubIcon size='1.5rem' />
-                    <span>Continue with GitHub</span>
-                </Button>
+                <Link href='/login'>
+                    <Button className='flex gap-2 button-glow transition-all hover:scale-105 duration-300 pointer-events-auto'>
+                        <GithubIcon size='1.5rem' />
+                        <span>Continue with GitHub</span>
+                    </Button>
+                </Link>
             </div>
 
-            <ChevronDown className='pointer-events-auto cursor-pointer absolute left-1/2 top-[calc(100vh-2rem)] -translate-x-1/2 -translate-y-full animate-bounce' />
+            {/* <div className='pointer-events-auto cursor-pointer absolute left-1/2 top-[calc(100vh-1rem)] -translate-x-1/2 -translate-y-full'>
+                <ChevronDown className='animate-bounce' />
+            </div> */}
         </div>
+
+        {/* <div className='w-full grow flex flex-col gap-8 p-8 text-white z-20 items-center justify-center'>
+            <h4 className='font-extrabold text-2xl text-primary'>Overview</h4>
+            <div className='grid grid-cols-2 gap-24'>
+                <div className='max-w-lg leading-7'>
+                    Recieving and managing webhooks can sometimes be difficult. 
+                    Maybe you don&apos;t have a domain to recieve webhooks at.
+                    Maybe you just want to test locally. 
+                    Maybe you don&apos;t want to miss any data if your application goes down.
+                    Maybe you just want a <b>simple solution</b> to all of these problems.
+                </div>
+                <div className='max-w-lg leading-7'>
+                    Our service enables your applications to recieve data the way they want to recieve it.
+                    Set up a websocket connection in seconds and have data being sent your way!
+                </div>
+            </div>
+        </div> */}
     </div>;
 }
